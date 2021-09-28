772da7408b68f50220336e3c84b9a391c8976358
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-09-28 20:23:26 +0900

    apollo client complete

---
+++ server.js
@@ -0,0 +1,11 @@
+import { ApolloServer } from "apollo-server";
+import { typeDefs, resolvers } from "./schema";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
