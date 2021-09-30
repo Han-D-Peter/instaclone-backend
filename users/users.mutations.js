0d1d67a49365d6cba59483458f703a44a926b911
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-09-30 12:26:19 +0900

    see profile & login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -7,32 +8,56 @@ export default {
       _,
       { firstName, lastName, userName, email, password }
     ) => {
-      // check if username or email are already on DB
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              userName,
-            },
-            {
-              email,
-            },
-          ],
-        },
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                userName,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        const user = await client.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        return user;
+      } catch (e) {
+        return console.log(e);
+      }
+    },
+    login: async (_, { userName, password }) => {
+      // find user with args.username
+      const user = await client.user.findFirst({
+        where: { userName },
       });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      const user = await client.user.create({
-        data: {
-          userName,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      return user;
-      //hash password
-      //save and return the user
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
     },
   },
 };
