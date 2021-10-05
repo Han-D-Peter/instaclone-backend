4c9100d40edf6d7300207cc5e137d849f5231f86
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-10-05 18:40:56 +0900

    photos& hashtag

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -2,15 +2,17 @@ import { gql } from "apollo-server";
 
 export default gql`
   type User {
-    id: String!
+    id: Int!
     firstName: String!
     lastName: String
     userName: String!
     email: String!
     bio: String
     avatar: String
+    photos: [Photo]
     following: [User]
     followers: [User]
+    photos: [Photo]
     totalFollowing: Int!
     totalFollowers: Int!
     isFollowing: Boolean!
