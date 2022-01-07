3b78a142e9dab51526ebf03ea6b7cd7d648a3248
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-01-07 16:32:01 +0900

    fix

--- src/photos/seeFeed/seeFeed.resolvers.js
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -3,8 +3,10 @@ import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Query: {
-    seeFeed: protectedResolver((_, __, { loggedInUser }) =>
-      client.photo.findMany({
+    seeFeed: protectedResolver((_, { offset }, { loggedInUser }) => {
+      return client.photo.findMany({
+        take: 2,
+        skip: offset,
         where: {
           OR: [
             {
@@ -24,7 +26,7 @@ export default {
         orderBy: {
           createdAt: "desc",
         },
-      })
-    ),
+      });
+    }),
   },
 };
